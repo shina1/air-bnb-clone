306a84156fb678e2bc576fbe91fee20681396123
shina1 <oswasiu@gmail.com>
2022-10-21 10:54:53 +0100

    implemeted custom reusable layout

---
+++ src/layouts/customStyle.ts
@@ -0,0 +1,41 @@
+// custom carousel styles
+
+export const  carouselD = {
+    color: '#fff',
+    opcatity: 0.5,
+    p: 1,
+    minWidth: 'auto',
+    backgroundColor: '#000',
+    borderRadius: 10,
+};
+
+export const carouselImg = {
+    display: 'block',
+    width: '100%',
+    height: 275,
+    borderRadius: 3
+};
+
+export const fixedIcon = {
+    postion: 'absolute',
+    right: 10,
+    top: 10,
+    zIndex: 10,
+};
+
+// desktop display
+
+export const deskDisplay = {
+    display: {
+        xs: 'none',
+        md: 'block'
+    }
+};
+
+// fixed button
+
+export const fixedBtn = {
+    position: 'absolute',
+    bottom: 100,
+    width: '100%'
+}
